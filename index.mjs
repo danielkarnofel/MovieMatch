0e96ae5e3562f7d03599c8b0a061f19457d510cb
Daniel <karnofeldaniel@protonmail.com>
2025-07-30 15:33:25 -0700

    Added db info

--- index.mjs
+++ index.mjs
@@ -10,26 +10,26 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
 // setting up database connection pool
-// const pool = mysql.createPool({
-//     host: "your_hostname",
-//     user: "your_username",
-//     password: "your_password",
-//     database: "your_database",
-//     connectionLimit: 10,
-//     waitForConnections: true
-// });
-// const conn = await pool.getConnection();
+const pool = mysql.createPool({
+    host: "danielkarnofel.tech",
+    user: "danielk1_webuser",
+    password: "CSUMB-cst336",
+    database: "danielk1_moviematch",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+const conn = await pool.getConnection();
 
 //routes
 app.get('/', (req, res) => {
    res.render('index');
 });
 
-// app.get("/dbTest", async(req, res) => {
-//     let sql = "SELECT CURDATE()";
-//     const [rows] = await conn.query(sql);
-//     res.send(rows);
-// }); // dbTest
+app.get("/dbTest", async(req, res) => {
+    let sql = "SELECT CURDATE()";
+    const [rows] = await conn.query(sql);
+    res.send(rows);
+});
 
 app.listen(3000, ()=>{
     console.log("Express server running")
