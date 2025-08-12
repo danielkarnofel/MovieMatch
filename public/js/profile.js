3f2b9d107816f057735e005d5fd1ea285ced1078
Daniel <karnofeldaniel@protonmail.com>
2025-08-12 14:43:37 -0700

    Added profile styling

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@ function toggleEditForm(id) {
     const form = document.getElementById(`edit-form-${id}`);
     
     if (form.style.display === "none" || form.style.display === "") {
-        form.style.display = "block";
+        form.style.display = "flex";
     } else {
         form.style.display = "none";
     }
